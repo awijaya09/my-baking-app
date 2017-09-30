2128be7be5f9a198c3bba1ab788e0f6d23a00925
Andree Wijaya <andree@twiscode.com>
2017-10-01 00:48:46 +0800

    - created basic files n widgets - created basic files on unit testing the main activity

---
+++ app/src/androidTest/java/com/awijaya/mybakingapp/LoadDataTest.java
@@ -0,0 +1,23 @@
+package com.awijaya.mybakingapp;
+
+/**
+ * Created by awijaya on 10/1/17.
+ */
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoadDataTest {
+    @Rule public ActivityTestRule<MainActivity> mainActivityActivityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void loadDataFromNetwork(){
+        
+    }
+
+}
