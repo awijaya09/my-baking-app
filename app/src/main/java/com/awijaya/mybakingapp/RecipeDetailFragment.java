987a666c0dff5970ce982e7800113527e8ca5da5
Andree Wijaya <andree@twiscode.com>
2017-10-05 17:18:57 +0700

    updated to recyclerview for detail recipe added functionality to resume player

--- app/src/main/java/com/awijaya/mybakingapp/RecipeDetailFragment.java
+++ app/src/main/java/com/awijaya/mybakingapp/RecipeDetailFragment.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.awijaya.mybakingapp.Model.Ingredient;
 import com.awijaya.mybakingapp.Model.Recipe;
@@ -36,10 +39,11 @@ public class RecipeDetailFragment extends Fragment {
     private static final String RECIPE_KEY = "recipe";
     public static final String INDEX_KEY = "curIndex";
     public static final String ING_KEY = "ingKey";
+    public static final String PLAYER_POS = "playerPos";
     private int curIndex = 0;
 
-    @BindView(R.id.list_view_recipe_steps)
-    ListView mListViewSteps;
+    @BindView(R.id.rv_detail_list)
+    RecyclerView mRecyclerViewDetail;
 
     @BindView(R.id.text_view_steps_title)
     TextView mStepTitle;
@@ -51,9 +55,11 @@ public class RecipeDetailFragment extends Fragment {
     Button mNextBtn;
 
     private Recipe mRecipe;
-    private RecipeDetailListViewAdapter mAdapter;
+    private LinearLayoutManager mAdapter;
+    private RecipeDetailListAdapter recipeAdapter;
     private ArrayList<Step> mSteps;
     private ArrayList<Ingredient> mIngredients;
+    private long curPlayerPos = 0l;
 
     public RecipeDetailFragment(){
 
@@ -67,11 +73,17 @@ public class RecipeDetailFragment extends Fragment {
         if (savedInstanceState != null) {
             mRecipe = savedInstanceState.getParcelable(RECIPE_KEY);
             curIndex = savedInstanceState.getInt(INDEX_KEY);
+            curPlayerPos = savedInstanceState.getLong(PLAYER_POS);
+            Toast.makeText(getContext(), "Saved Position: " + curPlayerPos, Toast.LENGTH_SHORT).show();
         }
 
-        Log.d(TAG, "onCreateView: curIndex value: " + curIndex);
-        mAdapter = new RecipeDetailListViewAdapter(getContext(), mRecipe, curIndex);
-        mListViewSteps.setAdapter(mAdapter);
+        mAdapter = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recipeAdapter = new RecipeDetailListAdapter(getContext(), mRecipe);
+        recipeAdapter.setPlayerCurrentPost(curPlayerPos);
+        mRecyclerViewDetail.setAdapter(recipeAdapter);
+        mRecyclerViewDetail.setLayoutManager(mAdapter);
+        mRecyclerViewDetail.setHasFixedSize(true);
+
         mSteps = mRecipe.recipeSteps;
         mIngredients = mRecipe.recipeIngredients;
 
@@ -81,6 +93,7 @@ public class RecipeDetailFragment extends Fragment {
             mPrevBtn.setEnabled(false);
         }
         setNextBtnClick();
+        setPrevBtnClick();
         return rootView;
     }
 
@@ -90,9 +103,24 @@ public class RecipeDetailFragment extends Fragment {
             public void onClick(View view) {
                 curIndex++;
                 Uri mediaUri = Uri.parse(mSteps.get(curIndex).stepVideoURL);
-                mAdapter.playNextVideo(mediaUri);
+                recipeAdapter.playNextVideo(mediaUri);
 
                 updateStepsTitle(mSteps.get(curIndex));
+            }
+        });
+    }
+
+    private void setPrevBtnClick() {
+        mPrevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (curIndex > 0) {
+                    curIndex--;
+                    Uri mediaUri = Uri.parse(mSteps.get(curIndex).stepVideoURL);
+                    recipeAdapter.playNextVideo(mediaUri);
+
+                    updateStepsTitle(mSteps.get(curIndex));
+                }
             }
         });
     }
@@ -111,12 +139,18 @@ public class RecipeDetailFragment extends Fragment {
         outState.putParcelable(RECIPE_KEY, mRecipe);
         outState.putInt(INDEX_KEY, curIndex);
         outState.putParcelableArrayList(ING_KEY, mIngredients);
+        outState.putLong(PLAYER_POS, recipeAdapter.getPlayerCurrentPost());
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        mAdapter.releasePlayer();
-        Log.d(TAG, "onDestroyView: The view has been destroyed");
+        recipeAdapter.releasePlayer();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        recipeAdapter.releasePlayer();
     }
 }
---
+++ app/src/main/java/com/awijaya/mybakingapp/RecipeDetailListAdapter.java
@@ -0,0 +1,298 @@
+package com.awijaya.mybakingapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.BoolRes;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.awijaya.mybakingapp.Model.Recipe;
+import com.awijaya.mybakingapp.Model.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by awijaya on 10/5/17.
+ */
+
+public class RecipeDetailListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements ExoPlayer.EventListener {
+
+    private ArrayList<Step> mSteps = new ArrayList<Step>();
+    private Recipe mRecipe;
+    private int stepIndex = 0;
+
+    private MediaSessionCompat mMediaSession;
+    private SimpleExoPlayer mExoPlayer;
+    private PlaybackStateCompat.Builder mStateBuilder;
+    private Context mContext;
+    private long mLastPosition = 0l;
+    private static final String MEDIA_TAG = "Media Session";
+    private static final String TAG = "Recipe Detail Adapter";
+
+    public RecipeDetailListAdapter (Context context, Recipe item) {
+        this.mRecipe = item;
+        this.mSteps = item.recipeSteps;
+        this.mContext = context;
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        Boolean shouldAttachToParent = false;
+        switch (viewType) {
+            case 0:
+                int layoutIdForItem = R.layout.recipe_detail_video;
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View view = inflater.inflate(layoutIdForItem, parent, shouldAttachToParent);
+                return new RecipeVideoViewHolder(view);
+            case 1:
+                int titleLayoutId = R.layout.recipe_detail_steps;
+                LayoutInflater titleInflater = LayoutInflater.from(context);
+                View titleView = titleInflater.inflate(titleLayoutId, parent, shouldAttachToParent);
+                return new RecipeDetailTitleViewHolder(titleView);
+            case 2:
+                int layoutId = R.layout.recipe_detail_ingredients;
+                LayoutInflater inf = LayoutInflater.from(context);
+                View stepView = inf.inflate(layoutId, parent, shouldAttachToParent);
+                return new RecipeDetailListViewHolder(stepView);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = holder.getItemViewType();
+        Log.d(TAG, "onBindViewHolder: viewtype: " + viewType);
+        switch (viewType){
+            case 0:
+                Uri mediaURI = Uri.parse(mRecipe.recipeSteps.get(stepIndex).stepVideoURL);
+                RecipeVideoViewHolder videoHolder = (RecipeVideoViewHolder) holder;
+                initializeMediaSession();
+                initializePlayer(videoHolder.mSimpleExoPlayer, mediaURI);
+                return;
+            case 2:
+                Step stepItem = mRecipe.recipeSteps.get(position-2);
+                RecipeDetailListViewHolder recipeDetail = (RecipeDetailListViewHolder) holder;
+                recipeDetail.mIngQty.setText(stepItem.stepShortDescription);
+                recipeDetail.mIngMeasure.setText(stepItem.stepDescription);
+                return;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mSteps.size()+2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position > 1) {
+            return 2;
+        } else {
+            return position;
+        }
+    }
+
+
+    class RecipeDetailListViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.text_view_ingredient_item_qty)
+        TextView mIngQty;
+
+        @BindView(R.id.text_view_ingredient_item_measure)
+        TextView mIngMeasure;
+
+        public RecipeDetailListViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    class RecipeDetailTitleViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.text_view_steps_title)
+        TextView mStepTitle;
+
+        public RecipeDetailTitleViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class RecipeVideoViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.simple_exoplayer)
+        SimpleExoPlayerView mSimpleExoPlayer;
+
+        public RecipeVideoViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public void releasePlayer(){
+        if (mExoPlayer != null){
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+
+    }
+
+    public void playNextVideo(Uri mediaUri){
+        mExoPlayer.stop();
+
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        DefaultDataSourceFactory defaultDataSourceFactory = new DefaultDataSourceFactory(mContext, "BakingMedia");
+
+        MediaSource mediaSource = new ExtractorMediaSource(mediaUri, defaultDataSourceFactory, extractorsFactory, null, null );
+        mExoPlayer.prepare(mediaSource);
+        mExoPlayer.setPlayWhenReady(true);
+
+    }
+
+
+    private void initializeMediaSession() {
+        mMediaSession = new MediaSessionCompat(mContext, TAG);
+        mMediaSession.setMediaButtonReceiver(null);
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE
+                );
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+        mMediaSession.setCallback(new RecipeDetailListAdapter.MySessionCallback());
+
+        mMediaSession.setActive(true);
+    }
+
+    private void initializePlayer(SimpleExoPlayerView mSimpleExoPlayerView, Uri mediaUri) {
+        if(mExoPlayer == null){
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector);
+            mSimpleExoPlayerView.setPlayer(mExoPlayer);
+
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            DefaultDataSourceFactory defaultDataSourceFactory = new DefaultDataSourceFactory(mContext, "BakingMedia");
+
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, defaultDataSourceFactory, extractorsFactory, null, null );
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+
+        @Override
+        public void onPlay() {
+            mExoPlayer.seekTo(mLastPosition);
+            mExoPlayer.setPlayWhenReady(true);
+
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            mLastPosition = mExoPlayer.getCurrentPosition();
+            mExoPlayer.setPlayWhenReady(false);
+            mExoPlayer.release();
+        }
+
+    }
+
+    public long getPlayerCurrentPost(){
+        return mExoPlayer.getCurrentPosition();
+    }
+
+    public void setPlayerCurrentPost(long curPost){
+        this.mLastPosition = curPost;
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+}
